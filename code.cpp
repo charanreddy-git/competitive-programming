4858e39b09217a8e3d5fe4aca7655292354f7e27
CHARAN REDDY <charanreddy.puchala@gmail.com>
2025-03-01 00:44:08 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int long long
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tt;
+    cin >> tt;
+
+    while(tt--){
+        
+    }
+    return 0;
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ -std=c++20 -o bin code.cpp  && ./bin < input.txt > output.txt
