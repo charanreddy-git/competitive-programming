97f323907f4d8f6e9c6047129780cf4387f4f2f4
Charan Reddy <charanreddy.puchala@gmail.com>
2025-04-26 21:50:06 +0530

    Update code.cpp

--- C++/code.cpp
+++ C++/code.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
-
-using namespace std;
+using namespace std; 
 #define int long long
 
+void solve(){
+    
+}
+
 int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int tt;
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int tt; 
     cin >> tt;
-
-    while(tt--){
-        
-    }
+    while(tt--) solve();
     return 0;
 }
