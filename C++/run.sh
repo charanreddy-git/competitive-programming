a4f26da69dd31e9e41b928592eac23ddb8f8a90b
Charan Reddy <charanreddy.puchala@gmail.com>
2025-05-29 00:24:31 +0530

    Update run.sh

--- C++/run.sh
+++ C++/run.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+# 1. compile + run + input from input.txt and output in output.txt
 g++ -std=c++20 -o bin code.cpp  && ./bin < input.txt > output.txt
+# 2. compile + run 
+g++ -std=c++20 -o bin code.cpp  && ./bin
+
+# comment other one while using one 
